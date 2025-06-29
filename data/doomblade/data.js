5473345af651c9e7fccfbc46dfb57e030057f181
Chatouille <chatouillegg@gmail.com>
2025-06-29 04:37:41 +0200

    add map for DOOMBLADE (tiles only atm)

---
+++ data/doomblade/data.js
@@ -0,0 +1,34 @@
+window.jsonp_mapdata = {
+	tilesets: [{
+		grid: [
+			[1],
+		],
+		sizeW: 0,
+		sizeE: 1,
+		sizeN: 0,
+		sizeS: 1,
+		zoomLevel: 100,
+		tileWidth: 1776,
+		tileHeight: 1492,
+		offsetX: 0,
+		offsetY: 0,
+	},{
+		grid: [
+			[1,1,1,1],
+			[1,1,1,1],
+			[1,1,1,1],
+			[1,1,1,1],
+		],
+		sizeW: 0,
+		sizeE: 4,
+		sizeN: 0,
+		sizeS: 4,
+		zoomLevel: 180,
+		tileWidth: 768,
+		tileHeight: 768,
+		offsetX: 100,
+		offsetY: 0,
+	}],
+	bgColor: "#050504",
+	icons: [],
+}
